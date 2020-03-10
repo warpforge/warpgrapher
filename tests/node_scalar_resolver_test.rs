a85bdd3e22a193458fd79023b82f8d88b0ff6e87
taylor <taylor.centers@gmail.com>
2020-03-10 19:12:28 -0400

    added float checks, added tests for non-array values too

--- tests/node_scalar_resolver_test.rs
+++ tests/node_scalar_resolver_test.rs
@@ -56,4 +56,49 @@ fn scalar_lists_test() {
 
     let floats = result.get("float_list").unwrap();
     assert!(floats.is_array());
+    assert_eq!(floats.get(0).unwrap().as_f64().unwrap(), 0.0);
+    assert_eq!(floats.get(1).unwrap().as_f64().unwrap(), 1.1);
+    assert_eq!(floats.get(2).unwrap().as_f64().unwrap(), 2.2);
+    assert_eq!(floats.get(3).unwrap().as_f64().unwrap(), 3.3);
+
+    assert!(server.shutdown().is_ok());
+}
+
+/// Passes if the create mutation and the read query both succeed.
+#[test]
+#[serial]
+fn scalar_lists_no_array_test() {
+    init();
+    clear_db();
+
+    let mut client = test_client();
+    let mut server = test_server("./tests/fixtures/scalar_list.yml");
+    assert!(server.serve(false).is_ok());
+
+    let result = client
+        .create_node(
+            "TestType",
+            "string_list
+             bool_list
+             int_list
+             float_list
+            ",
+            &json!({
+                "string_list": "string0",
+                "bool_list": false,
+                "int_list": 0,
+                "float_list": 0.0,
+            }),
+        )
+        .unwrap();
+
+    assert_eq!(result.get("string_list").unwrap().as_str().unwrap(), "string0");
+
+    assert_eq!(result.get("bool_list").unwrap().as_bool().unwrap(), false);
+
+    assert_eq!(result.get("int_list").unwrap().as_i64().unwrap(), 0);
+
+    assert_eq!(result.get("float_list").unwrap().as_f64().unwrap(), 0.0);
+
+    assert!(server.shutdown().is_ok());
 }
