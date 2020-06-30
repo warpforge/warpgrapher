66e1f201d672cd918e2844756c3535432f14c5d0
Jeffrey Howard <jhoward@alum.mit.edu>
2020-06-30 21:19:14 +0000

    Helps to get the environment variables right.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! Warpgrapher makes it painless to create web services with graph-based data
-//! models. Describe the data model for which you want to run a web service.
+//! models.  Describe the data model for which you want to run a web service.
 //! Wargrapher automatically generates a GraphQL schema from the data model, as
 //! well as a set of resolvers for basic create, read, update, and delete (CRUD)
 //! operations on that data. If you need more more sophisticated endpoints, you
