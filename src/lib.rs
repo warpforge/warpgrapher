9679a94dc409d839295b90b5629ef2993e37a22a
Jeffrey Howard <jhoward@alum.mit.edu>
2020-07-01 06:29:27 +0000

    Trigger pipeline>.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! models. Describe the data model for which you want to run a web service.
 //! Wargrapher automatically generates a GraphQL schema from the data model, as
 //! well as a set of resolvers for basic create, read, update, and delete (CRUD)
-//! operations on that data.  If you need more more sophisticated endpoints, you
+//! operations on that data. If you need more more sophisticated endpoints, you
 //! can supply your own custom resolvers. Warpgrapher will automatically
 //! generate the GraphQL configuration and invoke your custom resolvers when
 //! appropriate.
