d4f20e6b987481953260ad45041aa7a2443312e6
maldrake <16956831+maldrake@users.noreply.github.com>
2022-03-29 10:51:25 -0400

    Release Candidate v0.10.3 (#142)

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@
 //!
 //! To browse source code, report issues, or contribute to the project, see the [GitHub Repository](https://github.com/warpforge/warpgrapher).
 
-#![doc(html_root_url = "https://docs.rs/warpgrapher/0.10.2")]
+#![doc(html_root_url = "https://docs.rs/warpgrapher/0.10.3")]
 
 #[cfg(feature = "cypher")]
 pub use bolt_client;
